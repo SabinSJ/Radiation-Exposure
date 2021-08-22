73645a09988a56f679fa1b476e8b5493ad3291ae
Florin-Sabin Sarca <39478000+SabinSJ@users.noreply.github.com>
2021-08-22 12:35:00 +0300

    backend initial commit

---
+++ backend/src/test/java/com/radiationexposure/commons/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radiationexposure.commons.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

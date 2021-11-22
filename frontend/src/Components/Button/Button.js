09759f900ee7a827af566b787474ceffc75c5533
Florin-Sabin Sarca <39478000+SabinSJ@users.noreply.github.com>
2021-11-22 14:47:33 +0200

    refactoring Login/Logout Button

--- frontend/src/Components/Button/Button.js
+++ frontend/src/Components/Button/Button.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './css/Button.css';
+import '../css/Button.css';
 import {Link} from 'react-router-dom';
+import AuthenticationService from '../api/AuthenticationService';
 
 const STYLES = ['btn--primary','btn--outline']
 
@@ -12,11 +13,32 @@ export const Button = ({children,type,onClick,buttonStyle,buttonSize}) => {
 
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
+    let isLoggedIn = AuthenticationService.isUserLoggedIn();
+
     return (
+        <>
+        {
+
+        isLoggedIn
+
+        ?
+
+        <Link to = '/' className = 'btn-mobile'>
+            <button className = {`btn ${checkButtonStyle} ${checkButtonSize}`} onClick = {onClick} type = {type}>
+                {children}
+            </button>
+        </Link> 
+
+        :
+
         <Link to = '/login' className = 'btn-mobile'>
             <button className = {`btn ${checkButtonStyle} ${checkButtonSize}`} onClick = {onClick} type = {type}>
                 {children}
             </button>
         </Link> 
+
+        }
+
+        </>
     );
 };

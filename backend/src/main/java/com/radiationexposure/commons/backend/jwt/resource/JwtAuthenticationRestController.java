4f96b0ccbad4f57b94d73fb6ebc9d03d3b00d91e
Florin-Sabin Sarca <39478000+SabinSJ@users.noreply.github.com>
2021-11-30 15:27:11 +0200

    refactoring

--- backend/src/main/java/com/radiationexposure/commons/backend/jwt/resource/JwtAuthenticationRestController.java
+++ backend/src/main/java/com/radiationexposure/commons/backend/jwt/resource/JwtAuthenticationRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3000/")
+@CrossOrigin(origins="*")
 public class JwtAuthenticationRestController {
 
     @Autowired
